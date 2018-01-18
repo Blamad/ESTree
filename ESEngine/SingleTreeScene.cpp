1e1af503f3d3b990948487b35822d39831162a78
Eryk Sobczak <ersobcz@gmail.com>
2018-01-18 20:45:13 +0100

    Fixed parametric grammar

--- ESEngine/SingleTreeScene.cpp
+++ ESEngine/SingleTreeScene.cpp
@@ -43,17 +43,12 @@ void SingleTreeScene::initialize() {
 	bool normalVisualisation;
 	bool meshWiring;
 
-	paramsFileName = "LindenmayerRules/" + treeParams[7];
+	paramsFileName = "LindenmayerRules/" + treeParams[2];
 	leavesTextureName = "Textures/" + leavesParams[1];
 	barkMaterial = Material::bark1();
 	normalVisualisation = false;
 	meshWiring = false;	
 	go = createLindenmayerTree(paramsFileName, position, barkMaterial, Material::diffuseTextureOnly(leavesTextureName), meshWiring, normalVisualisation);
-
-	/*position = vec3(3, 0, 3);
-	paramsFileName = "LindenmayerRules/" + treeParams[1];
-	go = createLindenmayerTree(paramsFileName, position, barkMaterial, Material::diffuseTextureOnly(leavesTextureName), meshWiring, normalVisualisation);
-	*/
 
 	generateTestBox(vec3(8, 2, 8), vec3(0.5, 0.5, 0.5));
 
@@ -65,7 +60,6 @@ void SingleTreeScene::initialize() {
 	vec3 distance = vec3(-14);
 	createDirectionalLight(distance * dir, dir);
 
-	//setActiveCamera(createCamera(vec3(-20, 15, -35), 45, -10));
 	setActiveCamera(createCamera(vec3(0, 5, 15), -90, -10));
 
 	generateFrameBuffer();
